ad6c42d93c34f1af02717badd6493692f039ff01
ileana raijelson <ileraijelson@gmail.com>
2023-01-17 00:25:56 -0300

    corrigiendo errores

--- app.js
+++ app.js
@@ -1,45 +1,3 @@
-/*const verduras =[
-    {id:1,
-    nombre:"lechuga",
-    precio:100,
-    peso: "1kg",
-    img: "img/lechuga.png",
-    cantidad: 1,
-    },
-    {id:2,
-        nombre:"tomate",
-        precio:150,
-        peso: "1kg",
-        img: "img/tomate.png",
-        cantidad: 1, 
-    },
-    {id:3,
-        nombre:"zanahoria",
-        precio:120,
-        peso: "1kg",
-        img: "img/zanahoria.png",
-        cantidad: 1, 
-    },
-    {id:4,
-        nombre:"pepino",
-        precio:80,
-        peso: "1kg",
-        img: "img/pepino.png",
-        cantidad: 1, 
-    },
-]*/
-
-const fetchData = async () => {
-    fetch('verduras.json')
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-
-
-}) }
-
-fetchData();
-
 
 const contenedorCards = document.getElementById("contenedorCards")
 const verCarrito= document.getElementById("verCarrito")
@@ -47,65 +5,72 @@ const modalContainer= document.getElementById("modal-container")
 const cantidadCarrito =document.getElementById("cantidadCarrito")
 let carrito =JSON.parse(localStorage.getItem("carrito")) || [];
 
-verduras.forEach((verdu)=>{
-    let content= document.createElement("div");
-    content.className="card";
-    content.innerHTML=`
-    <img src="${verdu.img}" class="card-img-top">
-    <div class="card-body">
-    <h5 class="card-title">${verdu.nombre}</h5>
-    <p class="card-text">${verdu.peso} $${verdu.precio}</p>
-    </div>
-    
-    `;
-    contenedorCards.append(content);
-
-    let comprar = document.createElement("button")
-    comprar.innerText = "comprar";
-    comprar.className="btn btn-primary";
-
-    content.append(comprar);
-
-    comprar.addEventListener("click", ()=>{
-        Toastify({
-            text: "El producto se agrego correctamente",
-            duration: 3000,
-            newWindow: true,
-            close: true,
-            gravity: "top", 
-            position: "right", 
-            stopOnFocus: true, 
-            style: {
-            background: "linear-gradient(to right, green, green)",
-            },
-            onClick: function(){} 
-        }).showToast();
-        const repeat = carrito.some((repeatVerdu) => repeatVerdu.id === verdu.id);
-    
+const fetchData = async () => {
+	fetch("verduras.json")
+		.then((response) => response.json())
+		.then((verduras) => {
+			console.log(verduras);
+			verduras.forEach((verdu) => {
+				let content = document.createElement("div");
+				content.className = "card";
+				content.innerHTML = `
+        <img src="${verdu.img}" class="card-img-top">
+        <div class="card-body">
+        <h5 class="card-title">${verdu.nombre}</h5>
+        <p class="card-text">${verdu.peso} $${verdu.precio}</p>
+        </div>
         
-        
-        if(repeat){
-            carrito.map((verduras)=> {
-                if(verduras.id === verdu.id){
-                    verduras.cantidad++;
-                    
-                }
-            });
-        }else{
-            carrito.push({
-            id: verdu.id,
-            img: verdu.img,
-            peso: verdu.peso,
-            nombre: verdu.nombre,
-            precio: verdu.precio,
-            cantidad: verdu.cantidad,
-            });
-        }
-    carritoCounter();
-    GuardarLocal();
-    
-    });
-});
+        `;
+				contenedorCards.append(content);
+
+				let comprar = document.createElement("button");
+				comprar.innerText = "comprar";
+				comprar.className = "btn btn-primary";
+
+				content.append(comprar);
+
+				comprar.addEventListener("click", () => {
+					Toastify({
+						text: "El producto se agrego correctamente",
+						duration: 3000,
+						newWindow: true,
+						close: true,
+						gravity: "top",
+						position: "right",
+						stopOnFocus: true,
+						style: {
+							background: "linear-gradient(to right, green, green)",
+						},
+						onClick: function () {},
+					}).showToast();
+					const repeat = carrito.some(
+						(repeatVerdu) => repeatVerdu.id === verdu.id,
+					);
+
+					if (repeat) {
+						carrito.map((verduras) => {
+							if (verduras.id === verdu.id) {
+								verduras.cantidad++;
+							}
+						});
+					} else {
+						carrito.push({
+							id: verdu.id,
+							img: verdu.img,
+							peso: verdu.peso,
+							nombre: verdu.nombre,
+							precio: verdu.precio,
+							cantidad: verdu.cantidad,
+						});
+					}
+					carritoCounter();
+					GuardarLocal();
+				});
+			});
+		});
+};
+
+fetchData();
 
     const pintarCarrito= ()=>{
     modalContainer.innerHTML="";
